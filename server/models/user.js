ec8532a335c0ca2300e91a493f5e75ae0b450373
Anton Savoskin <anton@sliday.com>
2017-03-03 20:27:58 +0700

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -65,6 +65,21 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  const User = this;
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+    return bcrypt.compare(password, user.password).then((res) => {
+      if (res) {
+        return Promise.resolve(user);
+      }
+      return Promise.reject();
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   const user = this;
   if (user.isModified('password')) {
